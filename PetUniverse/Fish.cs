4e442c97d7be544698a2183aba4d1234905c1019
Leonardo Pérez <leo.prez.k@gmail.com>
2023-06-13 18:59:45 +0000

    Fish can't be cuddled silly!

---
+++ PetUniverse/Fish.cs
@@ -0,0 +1,26 @@
+﻿namespace PetUniverse;
+public class Fish : Pet
+{
+    public Fish(string name) : base(name) { }
+    public override string Speak()  
+    {
+        return "Glug";
+    }
+    public override bool Eat(string food)
+    {
+        if (food == "Fish food")
+        {
+            Love += 10;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public override int Cuddle()
+    {
+        Love -= 100;
+        return Love;
+    }
+}
