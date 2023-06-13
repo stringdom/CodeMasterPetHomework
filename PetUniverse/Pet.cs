50bdf7bc02fe107278d882949163a38cd1ad0ff1
Leonardo Pérez <leo.prez.k@gmail.com>
2023-06-13 16:31:23 +0000

    first commit

---
+++ PetUniverse/Pet.cs
@@ -0,0 +1,20 @@
+﻿namespace PetUniverse;
+public abstract class Pet
+{
+    public string Name { get; private set; }
+    public int Love { get; private set; } = 0;
+    public Pet(string name)
+    {
+        if (string.IsNullOrEmpty(name))
+        {
+            Name = "Invalid Name";
+        }
+        else
+        {
+            Name = name;
+        }
+    }
+    public abstract string Speak();
+    public abstract bool Eat();
+    public abstract int Cuddle();
+}
